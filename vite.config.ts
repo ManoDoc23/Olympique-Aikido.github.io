9db82aa8776451bc5fceea776623f020ba648d74
Marie-Noëlle Gos <gos.mn21@gmail.com>
2026-06-09 05:55:26 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Olympique-Aikido.github.io/", // <--- CETTE LIGNE DIT À GITHUB OÙ CHERCHER LES FICHIERS JS ET CSS
   build: {
-    // Ce joker empêche le build de planter si une image fait des caprices sur GitHub
     rollupOptions: {
       external: (id) => id.includes("kidsImg.jpg")
     }
