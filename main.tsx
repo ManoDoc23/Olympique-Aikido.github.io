dd1359731f86ef507e4d8589624560b1530f401d
Marie-Noëlle Gos <gos.mn21@gmail.com>
2026-09-11 20:25:20 +1100

    maj

--- main.tsx
+++ main.tsx
@@ -1,8 +1,9 @@
-import { createRoot } from "react-dom/client";
-import App from "./app/App.tsx";
-import "./styles/index.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App/App'  // ✅ Chemin vers App/App.tsx
 
-const root = document.getElementById("root");
-if (!root) throw new Error("Root element not found!");
-
-createRoot(root).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
