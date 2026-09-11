d99e5608ddf7832fb7e6f9eb1012167402216f8e
Marie-Noëlle Gos <gos.mn21@gmail.com>
2026-09-12 00:26:45 +1100

    Fix Tailwind CSS and update deployment workflow

--- main.tsx
+++ main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './app/App';
+import './styles//index'; // Ajustez le chemin selon l'emplacement de votre fichier CSS
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
