f5c114d051110a62f2cc5d89e431977be9149fbb
Marie-Noëlle Gos <gos.mn21@gmail.com>
2026-09-11 23:31:12 +1100

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './app/App'  // ✅ Chemin vers App/App.tsx
+import App from './app/App';
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
